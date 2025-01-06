e5b6f39ad3d9e0686c4ffaef353e44dbc1313fd6
valak70 <valaknoob@gmail.com>
2025-01-06 22:30:32 +0530

    minor bugs fixed

--- dream_vacation_project/settings.py
+++ dream_vacation_project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY', default='fallback-secret-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['dream-vacation-production.up.railway.app']
 
 
 # Application definition
